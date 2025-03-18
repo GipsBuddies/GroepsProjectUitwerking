4188eafe9bc24352eb1a81a33d0fa91d4e1b82c7
Sietse Versluis <sj.versluis1@student.avans.nl>
2025-03-18 18:19:26 +0100

    Added a working prototype with all visual elements in "Mainscene" within unity + added a working Screenhandler script with a temporarely bool which states if a user is logged in

---
+++ Assets/Scripts/VisualScripts/ScreenHandler.cs
@@ -0,0 +1,181 @@
+using UnityEngine;
+
+// This class contains methods used to navigate through the unity program.
+// All methods in the framework are named using the following convention: "GoTo(screen)".
+// This file contains a loggedIn boolean, which simulates a user being logged in or out.
+
+public class ScreenHandler : MonoBehaviour
+{
+    public GameObject startScreen;
+    public GameObject homeScreen;
+    public GameObject menubar;
+
+    public GameObject diaryScreen;
+    public GameObject writeDiaryScreen;
+    public GameObject readDiaryScreen;
+    public GameObject drawDiaryScreen;
+
+    public GameObject prephaseScreen;
+    public GameObject mainphaseScreen;
+    public GameObject postphasescreen;
+
+    public GameObject questionScreen;
+    public GameObject loginScreen;
+    public GameObject registerScreen;
+
+    public bool loggedIn;
+    private bool OnHomeScreen;
+
+
+    void Start()
+    {
+        GoToStartScreen();
+    }
+
+    public void ResetScreens()
+    {
+        startScreen.SetActive(false);
+        homeScreen.SetActive(false);
+        menubar.SetActive(false);
+
+        diaryScreen.SetActive(false);
+        writeDiaryScreen.SetActive(false);
+        readDiaryScreen.SetActive(false);
+        drawDiaryScreen.SetActive(false);
+
+        prephaseScreen.SetActive(false);
+        mainphaseScreen.SetActive(false);
+        postphasescreen.SetActive(false);
+
+        questionScreen.SetActive(false);
+        loginScreen.SetActive(false);
+        registerScreen.SetActive(false);
+
+        OnHomeScreen = false;
+    }
+
+    #region MainScenes
+
+    public void GoToStartScreen()
+    {
+        ResetScreens();
+        startScreen.SetActive(true);
+    }
+
+    public void GoToHomeScreen()
+    {
+        if (OnHomeScreen)
+        {
+            GoToStartScreen();
+            OnHomeScreen = false;
+        }
+        else
+        {
+            ResetScreens();
+            menubar.SetActive(true);
+            homeScreen.SetActive(true);
+            OnHomeScreen = true;
+        }
+    }
+
+    #endregion
+
+    #region DiaryScreens
+
+    public void GoToDiaryScreen()
+    {
+        if (loggedIn)
+        {
+            ResetScreens();
+            menubar.SetActive(true);
+            diaryScreen.SetActive(true);
+            writeDiaryScreen.SetActive(true);
+        }
+        else
+        {
+            GoToLoginScreen();
+        }
+    }
+
+    public void GoToReadDiaryScreen()
+    {
+        if (loggedIn)
+        {
+            ResetScreens();
+            menubar.SetActive(true);
+            diaryScreen.SetActive(true);
+            readDiaryScreen.SetActive(true);
+        }
+        else
+        {
+            GoToLoginScreen();
+        }
+    }
+
+    public void GoToDrawDiaryScreen()
+    {
+        if (loggedIn)
+        {
+            ResetScreens();
+            menubar.SetActive(true);
+            diaryScreen.SetActive(true);
+            drawDiaryScreen.SetActive(true);
+        }
+        else
+        {
+            GoToLoginScreen();
+        }
+    }
+
+    #endregion
+
+    #region PhaseScreens
+
+    public void GoToPrephaseScreen()
+    {
+        ResetScreens();
+        menubar.SetActive(true);
+        prephaseScreen.SetActive(true);
+    }
+
+    public void GoToMainphaseScreen()
+    {
+        ResetScreens();
+        menubar.SetActive(true);
+        mainphaseScreen.SetActive(true);
+    }
+
+    public void GoToPostphaseScreen()
+    {
+        ResetScreens();
+        menubar.SetActive(true);
+        postphasescreen.SetActive(true);
+    }
+
+    #endregion
+
+    #region OtherScreens
+
+    public void GoToQuestionScreen()
+    {
+        ResetScreens();
+        menubar.SetActive(true);
+        questionScreen.SetActive(true);
+    }
+
+    public void GoToLoginScreen()
+    {
+        ResetScreens();
+        menubar.SetActive(true);
+        loginScreen.SetActive(true);
+    }
+
+    public void GoToRegisterScreen()
+    {
+        ResetScreens();
+        menubar.SetActive(true);
+        registerScreen.SetActive(true);
+    }
+
+    #endregion
+}
